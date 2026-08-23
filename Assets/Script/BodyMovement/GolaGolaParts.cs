7c70d508e664d8d90aae9146903a18bdb22dcb5a
Appie-1019 <isihyeon005@gmail.com>
2026-08-24 02:00:11 +0900

    GolaGolaSimulator V1.4

--- Assets/Script/BodyMovement/GolaGolaParts.cs
+++ Assets/Script/BodyMovement/GolaGolaParts.cs
@@ -3,21 +3,35 @@ using UnityEngine;
 public class GolaGolaParts : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private Vector2 targetOffset;
     [SerializeField] private float angleOffset;
+    
+    [HideInInspector] public Vector3 offset;
+    private Quaternion rotation;
 
-    void Update()
+    private void Awake()
     {
-        if (target != null)
+        if (target == null)
         {
-            LookAtPos((Vector2)target.position + targetOffset);
+            Debug.LogError(gameObject.name + " 은(는) 필요한 컴포넌트가 없음");
+            return;
         }
+
+        offset = transform.position;
+        rotation = transform.rotation;
     }
 
-    private void LookAtPos(Vector2 pos)
+    public void LookAtBody() => LookAtPos((Vector2)target.position);
+    public void LookAtPos(Vector3 pos) => LookAtPos((Vector2)pos);
+    public void LookAtPos(Vector2 pos)
     {
         Vector2 dir = pos - (Vector2)transform.position;
         float targetAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + angleOffset;
         transform.rotation = Quaternion.Euler(0, 0, targetAngle);
+    }
+
+    public void ResetAll()
+    {
+        transform.position = offset;
+        transform.rotation = rotation;
     }
 }
